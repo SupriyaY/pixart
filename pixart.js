168599b2310fc24b885ea57b329cc6e19f146a1c
supriya  yerramilli <supriyayerramilli@supriyas-MacBook-Air.local>
2019-04-09 19:16:17 -0400

    attemping to refactor in jquery

--- pixart.js
+++ pixart.js
@@ -11,36 +11,50 @@
 // event.preventDefault();
 // })
 
+// for(let i = 0; i<8000; i++){
+// let newSquare = document.createElement('div')
+// newSquare.classList.add('square')
+// newSquare.addEventListener('mouseover', function() {
+// newSquare.style.backgroundColor = newColor
 
-
-
-let newColor = 'pink'
-
-document.getElementById('set-color').addEventListener('click', function(){
-newColor = document.getElementById('color-field').value
-document.getElementsByClassName('brush')[0].style.backgroundColor = newColor;
-event.preventDefault() 
-})
-
-
-
- for(let i = 0; i<8000; i++){
-let newSquare= document.createElement('div')
-newSquare.classList.add("square")
-newSquare.addEventListener('mouseover', function(){
-newSquare.style.backgroundColor = newColor
-    
-})
-document.body.appendChild(newSquare);
-
-}
-
-
-
-
-
-
-// paint.addEventListener('click', function(){
-// paint.style.backgroundColor = newColor;
 // })
+// document.body.appendChild(newSquare);
+// }
+
+//jQuery
+//When I type a value into the input field, it change the display of the div to the color of
+
+let newValue = 'pink';
+
+$('#set-color').click(function() {
+newValue = $('#color-field').val()
+// $('.brush').css('backgroundColor') = newValue
+event.preventDefault();
+})
+
+
+alert(newValue);
+
+// let newColor = 'pink'
+
+// document.getElementById('set-color').addEventListener('click', function(){
+// newColor = document.getElementById('color-field').value
+// document.getElementsByClassName('brush')[0].style.backgroundColor = newColor;
+// event.preventDefault() 
+// })
+
+
+
+//  for(let i = 0; i<8000; i++){
+// let newSquare= document.createElement('div')
+// newSquare.classList.add("square")
+// newSquare.addEventListener('mouseover', function(){
+// newSquare.style.backgroundColor = newColor
+    
+// })
+// document.body.appendChild(newSquare);
+
+// }
+
+
 
