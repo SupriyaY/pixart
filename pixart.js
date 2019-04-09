877d8a157a71e608ed5452636651595e2592e10b
supriya  yerramilli <supriyayerramilli@supriyas-MacBook-Air.local>
2019-04-09 13:14:08 -0400

    part 1

---
+++ pixart.js
@@ -0,0 +1,27 @@
+//when I input a value into the field, the div should change to the value I input
+
+
+//JS with querySelectors
+
+// let newColor = 'pink'
+
+// document.querySelector('#set-color').addEventListener('click',()=>{
+// newColor = document.querySelector('#color-field').value
+// document.querySelector('.brush').style.backgroundColor = newColor;
+// event.preventDefault();
+// })
+
+
+
+
+let newColor = 'pink'
+
+document.getElementById('set-color').addEventListener('click',function(){
+newColor = document.getElementById('color-field').value
+document.getElementsByClassName('brush')[0].style.backgroundColor = newColor;
+event.preventDefault() 
+})
+
+
+
+
